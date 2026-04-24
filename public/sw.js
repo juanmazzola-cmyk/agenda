e9dc08467393e62bf5065973a9307af679535907
Juan Mazzola <juan.mazzola@gmail.com>
2026-04-23 21:01:15 -0300

    SW: forzar red en navegación, evitar HTML cacheado

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        self.clients.claim().then(() => {
+            return self.clients.matchAll({ type: 'window' });
+        }).then(clients => {
+            clients.forEach(client => client.navigate(client.url));
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+    if (event.request.mode === 'navigate') {
+        event.respondWith(fetch(event.request, { cache: 'no-store' }));
+    } else {
+        event.respondWith(fetch(event.request));
+    }
 });
