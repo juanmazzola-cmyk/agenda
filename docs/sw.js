7ed13fde172b5dec17a28f9e79f8cf72d47e21ae
Juan Mazzola <juan.mazzola@gmail.com>
2026-05-06 22:10:05 -0300

    fix: mostrar importe en historial para turnos del dia actual (fecha <= hoy)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-v48';
+const CACHE = 'agenda-v49';
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png'];
 const DYNAMIC = ['./', './index.html', './app.js'];
 
